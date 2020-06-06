b1c83a5242f56ca24a2a1f918f433e47f928d002
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2020-06-07 00:24:01 +0300

    @Kommas (PyCharm Commit and Push Code Lines)

--- ΕΛΕΓΧΟΣ_INTERNET_ΣΤΑ_ΚΑΤΑΣΤΗΜΑΤΑ/app.py
+++ ΕΛΕΓΧΟΣ_INTERNET_ΣΤΑ_ΚΑΤΑΣΤΗΜΑΤΑ/app.py
@@ -1,6 +1,7 @@
 import subprocess
+import re
 from Private import slack_app, stores_sensitive_info
-#
+
 
 class Store:
     def __init__(self, name, area, ip):
@@ -8,14 +9,22 @@ class Store:
         self.area = area
         self.ip = ip
         self.ping = 'ΑΓΝΩΣΤΟ'
+        self.response_time = 'None'
+
+    def __str__(self):
+        return f'ΚΑΤΑΣΤΗΜΑ: {self.name} || ΠΕΡΙΟΧΗ: {self.area} || ΚΑΤΑΣΤΑΣΗ: {self.ping} || ΑΠΟΚΡΙΣΗ: {self.response_time} ms'
 
     def check_internet_status(self):
-        command = ['ping', '-c', '1', self.ip]
-        if subprocess.call(command) == 0:
-            self.ping = 'ONLINE'
-        else:
-            self.ping = 'OFFLINE'
-        return f'ΚΑΤΑΣΤΗΜΑ: {self.name} || ΠΕΡΙΟΧΗ: {self.area} || ΚΑΤΑΣΤΑΣΗ: {self.ping} \n'
+        command = ['ping', '-c', '4', self.ip]
+        try:
+            output = subprocess.check_output(command)
+            output = output.decode('utf8')
+            statistic = re.search(r'(\d+\.\d+/){3}\d+\.\d+', output).group(0)
+            avg_time = re.findall(r'\d+\.\d+', statistic)[1]
+            self.response_time = float(avg_time)
+        except subprocess.CalledProcessError:
+            pass
+        self.ping = ('ONLINE' if subprocess.call(command) == 0 else 'OFFLINE')
 
 
 ELOUNDA_MARKET = Store(name='Elounda Market',
@@ -33,11 +42,15 @@ LATO_02 = Store(name='Lato 02',
                 ip=stores_sensitive_info.ip['Lato 02']
                 )
 
+ELOUNDA_MARKET.check_internet_status()
+LATO_01.check_internet_status()
+LATO_02.check_internet_status()
+
 # ----------------SLACK BOT----------------------------
 output = f"""
 ΕΛΕΓΧΟΣ INTERNET ΣΤΑ ΚΑΤΑΣΤΗΜΑΤΑ
-{ELOUNDA_MARKET.check_internet_status()}
-{LATO_01.check_internet_status()}
-{LATO_02.check_internet_status()}
+{ELOUNDA_MARKET}
+{LATO_01}
+{LATO_02}
 """
 slack_app.post_message_to_slack(output)
