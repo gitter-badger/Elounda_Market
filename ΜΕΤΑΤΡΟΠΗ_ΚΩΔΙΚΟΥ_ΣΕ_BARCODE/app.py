e9cc0c12b72c5901fe8d335008371d3824f0f100
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2020-06-07 17:23:16 +0300

    @Kommas (PyCharm Commit and Push Code Lines)

---
+++ ΜΕΤΑΤΡΟΠΗ_ΚΩΔΙΚΟΥ_ΣΕ_BARCODE/app.py
@@ -0,0 +1,35 @@
+#  Copyright (c) 2020. Ioannis E. Kommas. All Rights Reserved
+
+# -------------- READ WRITE FILES ----------------------
+with open('write.txt', 'w') as a:
+    print('[BARCODE]', file=a)
+with open("read.txt", 'r') as q:
+    line = q.read().splitlines()
+stathero = '5200310'
+
+
+# Mayogiannis = 5200310
+# Koukoutaris = 5202535
+
+# ---------------FUNCTION HERE---------------------
+def calculate(code):
+    code = stathero + code
+    athrisma = 0
+    for y in range(len(code)):
+        if y % 2 == 0:
+            athrisma += int(code[y]) * 1
+        else:
+            athrisma += int(code[y]) * 3
+    micro = athrisma % 10
+    if micro == 0:
+        last_digit = 0
+    else:
+        last_digit = 10 - micro
+    barcode = code + str(last_digit)
+    return barcode
+
+
+# --------------MAIN PROGRAM----------------------
+for z in line:
+    with open('write.txt', 'a') as a:
+        print(calculate(z), file=a)
