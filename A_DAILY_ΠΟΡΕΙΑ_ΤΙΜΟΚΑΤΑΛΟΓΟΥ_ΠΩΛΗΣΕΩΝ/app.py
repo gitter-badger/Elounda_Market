749b855a971f847ede868370e95a7400980ea59b
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2020-06-24 12:44:58 +0300

    @Ioannis E. Kommas (PyCharm Commit and Push Code Lines)

--- A_DAILY_ΠΟΡΕΙΑ_ΤΙΜΟΚΑΤΑΛΟΓΟΥ_ΠΩΛΗΣΕΩΝ/app.py
+++ A_DAILY_ΠΟΡΕΙΑ_ΤΙΜΟΚΑΤΑΛΟΓΟΥ_ΠΩΛΗΣΕΩΝ/app.py
@@ -4,12 +4,12 @@ from pathlib import Path
 from A_DAILY_ΠΟΡΕΙΑ_ΤΙΜΟΚΑΤΑΛΟΓΟΥ_ΠΩΛΗΣΕΩΝ.library import sql_select, timokatalogos, excel_export
 from Private import sql_connect, slack_app
 import matplotlib.pyplot as plt
+import time
+from _datetime import datetime as dt
 
 # ----------------MAKE DF REPORT VIEWABLE----------------------------
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
-
-
 
 # -------------------- STATEMENTS HERE --------------------
 choose = timokatalogos.lista_2020[-1]
@@ -21,64 +21,97 @@ id = choose.id
 BASE_DIR = Path(__file__).resolve(strict=True).parents[1]
 path_to_file = BASE_DIR / f'A_DAILY_ΠΟΡΕΙΑ_ΤΙΜΟΚΑΤΑΛΟΓΟΥ_ΠΩΛΗΣΕΩΝ/excel/{id}.xlsx'
 
-# -------------------- ΔΙΑΒΑΖΩ ΤΟΝ ΤΙΜΟΚΑΤΑΛΟΓΟ --------------------
-timokatalogos = pd.read_sql_query(sql_select.get_products_in_the_period(from_date, to_date), sql_connect.sql_cnx())
 
-# -------------------- READ SALES --------------------
-sales = pd.read_sql_query(sql_select.get_sales(from_date, to_date, tuple(timokatalogos['ΚΩΔΙΚΟΣ'].values)),
-                          sql_connect.sql_cnx())
+while True:
 
-# -------------------- MERGE RESULTS --------------------
-final_result = pd.merge(left=timokatalogos, right=sales, left_on='ΚΩΔΙΚΟΣ', right_on='ΚΩΔΙΚΟΣ').sort_values(
-    by=['SalesQuantity'])
+    # -------------------- ΔΙΑΒΑΖΩ ΤΗΝ ΤΕΛΕΥΤΑΙΑ ΤΙΜΗ ΤΖΙΡΟΥ --------------------
+    with open('tziros.txt', 'r') as file:
+        tziros = float(file.read())
 
-# --------------------GROUP BY BRANDS TO SLACK --------------------
-brand_sales = final_result[['BRAND', 'SalesQuantity', 'Turnover']].groupby(by='BRAND').sum() \
-    .sort_values('BRAND').reset_index()
+    # -------------------- ΔΙΑΒΑΖΩ ΤΟΝ ΤΙΜΟΚΑΤΑΛΟΓΟ --------------------
+    timokatalogos = pd.read_sql_query(sql_select.get_products_in_the_period(from_date, to_date), sql_connect.sql_cnx())
 
-# -------------OPEN FILE | WRITE ----------------------------
-excel_export.export(path_to_file, final_result)
+    # -------------------- READ SALES --------------------
+    sales = pd.read_sql_query(sql_select.get_sales(from_date, to_date, tuple(timokatalogos['ΚΩΔΙΚΟΣ'].values)),
+                              sql_connect.sql_cnx())
 
-# -------------------- PLOT --------------------
-plt.figure(figsize=(15, 9))
-plt.subplot(xlabel='Brand', title=f'{choose.comments} [ΕΝΑΡΞΗ: {from_date.strftime("%d-%m")} - ΛΗΞΗ: {to_date.strftime("%d-%m")}]')
-plt.bar(brand_sales.BRAND, brand_sales.Turnover, alpha=0.5, color='red', label='ΤΖΙΡΟΣ')
-plt.plot(brand_sales.BRAND, brand_sales.SalesQuantity, alpha=0.5, color='blue', label='ΠΟΣΟΤΗΤΑ', marker='o',
-         linestyle="None")
-for x, y in zip(brand_sales.BRAND, brand_sales.SalesQuantity):
-    label = "{:.2f} TEM".format(y)
+    # -------------------- MERGE RESULTS --------------------
+    final_result = pd.merge(left=timokatalogos, right=sales, left_on='ΚΩΔΙΚΟΣ', right_on='ΚΩΔΙΚΟΣ').sort_values(
+        by=['SalesQuantity'])
 
-    # this method is called for each point
-    plt.annotate(label,  # this is the text
-                 (x, y),  # this is the point to label
-                 textcoords="offset points",  # how to position the text
-                 xytext=(0, 10),  # distance from text to points (x,y)
-                 ha='center')  # horizontal alignment can be left, right or center
-plt.grid(True, alpha=0.8)
-plt.legend()
-plt.savefig('views.png')
-plt.show()
+    # --------------------GROUP BY BRANDS TO SLACK --------------------
+    brand_sales = final_result[['BRAND', 'SalesQuantity', 'Turnover']].groupby(by='BRAND').sum() \
+        .sort_values('BRAND').reset_index()
 
-# -------------------- SLACK BOT DELETE (3 OLD POSTS) --------------------
-x = (slack_app.history(slack_app.channels_id[0]))
-for i in range(3):
-    timer = (x['messages'][i]['ts'])
-    slack_app.delete(slack_app.channels_id[0], timer)
+    # --------------------ΕΚΤΥΠΩΝΩ STATEMENT --------------------
+    print(f'{dt.now()} :CHECKING DIFFERENCES:')
 
-# -------------------- SLACK BOT ADD TEXT --------------------
-report = f"""
->ΠΟΡΕΙΑ ΠΩΛΗΣΕΩΝ ΓΙΑ ΤΙΣ ΠΡΟΣΦΟΡΕΣ:
-` ΣΥΜΜΕΤΕΧΟΥΝ: \t {len(final_result)} ΠΡΟΪΟΝΤΑ `
-` DATERANGE: \t ΑΠΟ: {from_date.strftime("%d-%m-%Y")} \t ΕΩΣ: {to_date.strftime("%d-%m-%Y")} `
-` ΠΟΣΟΤΗΤΑ ΠΩΛΗΣΕΩΝ: \t {final_result.SalesQuantity.sum()} TEM `
-` ΤΖΙΡΟΣ ΠΩΛΗΣΕΩΝ: \t {round(final_result.Turnover.sum(), 2)} € `
-` Α/Α ΕΝΕΡΓΕΙΑ: {choose.id}`
-` {choose.comments}`
-```{brand_sales}```
-"""
+    # --------------------ΕΝΑΡΞΗ ΕΛΕΓΧΟΥ --------------------
+    if tziros != round(final_result.Turnover.sum(), 2):
 
-slack_app.send_text(report, slack_app.channels[0])
+        # --------------------ΕΚΤΥΠΩΝΩ STATEMENT --------------------
+        print(f'{dt.now()} :FOUND NEW RECORDS:')
 
-# -------------------- SLACK BOT ADD FILES --------------------
-slack_app.send_files(f'{id}.xlsx', path_to_file, 'xlsx', slack_app.channels[0])
-slack_app.send_files('views.png', 'views.png', 'png', slack_app.channels[0])
+        # -------------OPEN FILE | WRITE ----------------------------
+        excel_export.export(path_to_file, final_result)
+
+        # -------------------- PLOT --------------------
+        plt.figure(figsize=(15, 9))
+        plt.subplot(xlabel='Brand',
+                    title=f'ΕΝΕΡΓΕΙΑ: {id}η || {choose.comments} || [ΕΝΑΡΞΗ: {from_date.strftime("%d-%m")} - ΛΗΞΗ: {to_date.strftime("%d-%m")}]')
+        plt.bar(brand_sales.BRAND, brand_sales.Turnover, alpha=0.5, color='red', label='ΤΖΙΡΟΣ')
+        plt.plot(brand_sales.BRAND, brand_sales.SalesQuantity, alpha=0.5, color='blue', label='ΠΟΣΟΤΗΤΑ', marker='o',
+                 linestyle="None")
+        for x, y in zip(brand_sales.BRAND, brand_sales.SalesQuantity):
+            label = "{:.2f} TEM".format(y)
+
+            # this method is called for each point
+            plt.annotate(label,  # this is the text
+                         (x, y),  # this is the point to label
+                         textcoords="offset points",  # how to position the text
+                         xytext=(0, 10),  # distance from text to points (x,y)
+                         ha='center')  # horizontal alignment can be left, right or center
+        plt.grid(True, alpha=0.8)
+        plt.legend()
+        plt.savefig('views.png')
+        plt.show()
+
+        # -------------------- SLACK BOT DELETE (3 OLD POSTS) --------------------
+        x = (slack_app.history(slack_app.channels_id[0]))
+        for i in range(3):
+            timer = (x['messages'][i]['ts'])
+            slack_app.delete(slack_app.channels_id[0], timer)
+
+        # -------------------- SLACK BOT ADD TEXT --------------------
+        report = f"""
+        >ΠΟΡΕΙΑ ΠΩΛΗΣΕΩΝ ΓΙΑ ΤΙΣ ΠΡΟΣΦΟΡΕΣ:
+        ` ΣΥΜΜΕΤΕΧΟΥΝ: \t {len(final_result)} ΠΡΟΪΟΝΤΑ `
+        ` DATERANGE: \t ΑΠΟ: {from_date.strftime("%d-%m-%Y")} \t ΕΩΣ: {to_date.strftime("%d-%m-%Y")} `
+        ` ΠΟΣΟΤΗΤΑ ΠΩΛΗΣΕΩΝ: \t {final_result.SalesQuantity.sum()} TEM `
+        ` ΤΖΙΡΟΣ ΠΩΛΗΣΕΩΝ: \t {round(final_result.Turnover.sum(), 2)} € `
+        ` Α/Α ΕΝΕΡΓΕΙΑ: {choose.id}`
+        ` {choose.comments}`
+        ```{brand_sales}```
+        """
+
+        slack_app.send_text(report, slack_app.channels[0])
+
+        # -------------------- SLACK BOT ADD FILES --------------------
+        slack_app.send_files(f'{id}.xlsx', path_to_file, 'xlsx', slack_app.channels[0])
+        slack_app.send_files('views.png', 'views.png', 'png', slack_app.channels[0])
+    else:
+
+        # --------------------ΕΚΤΥΠΩΝΩ STATEMENT --------------------
+        print(f'{dt.now()} :NOTHING TO REPORT:')
+
+    # --------------------WRITE TZIROS ON TXT --------------------
+    with open('tziros.txt', 'w') as file:
+        file.write(f'{round(final_result.Turnover.sum(), 2)}')
+
+    # --------------------ΕΚΤΥΠΩΝΩ STATEMENT --------------------
+    print(f'{dt.now()} :PAUSE ON:')
+    time.sleep(300)  # 5 minutes
+
+    # --------------------ΕΚΤΥΠΩΝΩ STATEMENT --------------------
+    print(f'{dt.now()} :PAUSE OFF:')
+
