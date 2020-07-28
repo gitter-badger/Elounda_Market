07b80414c1adb6c80afdd4dffe6efec3b4910a9f
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2020-07-28 11:44:55 +0300

    ``@Ioannis E. Kommas (PyCharm Commit and Push Code Lines)``

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 #   Copyright (c) 2020. Ioannis E. Kommas. All Rights Reserved
+def DecimalToBinary(num):
+    if num > 1:
+        DecimalToBinary(num // 2)
+    print(num % 2, end='')
 
-print(0 == 0)
-print([] == 0)
-print([] == '0')
+
+# Driver Code
+if __name__ == '__main__':
+    # decimal value
+    dec_val = 24
+
+    # Calling function
+    DecimalToBinary(dec_val)
