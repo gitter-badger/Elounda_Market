c1b3f8634d6579a210f12901794fea4b54f595be
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2020-09-19 23:14:38 +0300

    ``@Ioannis E. Kommas (PyCharm Commit and Push Code Lines)``

--- test.py
+++ test.py
@@ -23,4 +23,21 @@ print(round(katanalosi_low), round(katanalosi_high))
 print(487 *0.11028)
 print(487 *0.063)
 
+"""
+alphabet                                    αλφάβητο
+what's your name                            Πως σε λένε
+surname                                     επίθετο
+How do you spell it                         Πως γράφεται
+number                                      αριθμός
+What's your telephone number                Ποιό είναι το τηλέφωνό σου
+theory                                      θεωρία
+consonant                                   σύμφωνο
+vowel                                       φωνήεν
+alarm clock                                 ξυπνητήρι
+digital camera                              ψηφιακή φωτογραφική μηχανή
+agenda                                      αντζέντα
+scarf                                       φουλάρι
+pencil case                                 μολυβοθήκη
+glue                                        κόλλα   
 
+"""
