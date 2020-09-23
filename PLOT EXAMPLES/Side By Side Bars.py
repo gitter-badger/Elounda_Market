da9f706a1b637522f02546ad3d15e00e31e9c62a
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2020-09-24 00:18:14 +0300

    ``@Ioannis E. Kommas (PyCharm Commit and Push Code Lines)``

--- PLOT EXAMPLES/Side By Side Bars.py
+++ PLOT EXAMPLES/Side By Side Bars.py
@@ -19,7 +19,7 @@ def create_x(t, w, n, d):
 plt.figure(figsize=(10,8))
 school_a_x = create_x(2, .8, 1, 5)
 school_b_x = create_x(2, .8, 2, 5)
-middle_x = [ (a + b) / 2.0 for a, b in zip(school_a_x, school_b_x)]
+middle_x = [(a + b) / 2.0 for a, b in zip(school_a_x, school_b_x)]
 ax = plt.subplot(title='Test Averages on Different Units', xlabel='Unit', ylabel='Test Average')
 plt.bar(school_a_x, middle_school_a, label='Middle School A')
 plt.bar(school_b_x, middle_school_b, label='Middle School B')
--- test.py
+++ test.py
@@ -1,15 +1,1 @@
 #   Copyright (c) 2020. Ioannis E. Kommas. All Rights Reserved
-
-from matplotlib import pyplot as plt
-
-payment_method_names = ["Card Swipe", "Cash", "Apple Pay", "Other"]
-payment_method_freqs = [270, 77, 32, 11]
-
-plt.figure(figsize=(15, 9))
-plt.subplot(title='Retail Price [Scatter Plot]')
-plt.pie(payment_method_freqs,
-        autopct='%d%%',
-        labels=payment_method_names)
-plt.axis('equal')
-
-plt.show()
