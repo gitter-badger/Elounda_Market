93b91967bf4f93ac09a8e1ac15b847ed10586180
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2020-10-06 00:27:29 +0300

    ``@Ioannis E. Kommas (PyCharm Commit and Push Code Lines)``

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
 #   Copyright (c) 2020. Ioannis E. Kommas. All Rights Reserved
+
+import os
+import subprocess
+
+folder_to_delete = 'build'
+file = 'Discovery.spec'
+file_to_delete = f'{folder_to_delete}/{file}'
+
+folder_command = ['rmdir', folder_to_delete]
+file_command = ['rm', file_to_delete]
+base_dir = ['cd', os.path.abspath(os.getcwd())]
+
+subprocess.run(base_dir)
+
+subprocess.run(file_command)
+subprocess.run(folder_command)
+
